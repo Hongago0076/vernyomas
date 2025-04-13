5e181db137c77e8386173186efe7e62254c02539
Vágyi Mihály Adolf <Vagyi.Mihaly.Adolf@stud.u-szeged.hu>
2025-04-14 00:04:49 +0200

    git desktop hulyesege

---
+++ vernyomas-mero-app/src/app/directives/blood-pressure-status.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BloodPressureStatusDirective } from './blood-pressure-status.directive';
+
+describe('BloodPressureStatusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BloodPressureStatusDirective();
+    expect(directive).toBeTruthy();
+  });
+});
